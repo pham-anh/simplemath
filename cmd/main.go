33142c66f17f22fa10c6c0ff7ce03bb1f23e711f
Pham Quynh Anh <pqa.dev@gmail.com>
2025-09-17 21:37:00 +0900

    logger

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,7 @@ import (
 
 func main() {
 	e := echo.New()
+	e.Use(middleware.Logger())
 
 	// Seeded RNG for generation (inject into handler)
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
@@ -19,15 +20,17 @@ func main() {
 
 	// Apply the rate limiter middleware to the POST endpoint.
 	// We'll limit it to 1 request per second from each IP address.
-	e.POST("/", h.HandleSubmit, middleware.RateLimiterWithConfig(middleware.RateLimiterConfig{
+	ratelimit := middleware.RateLimiterWithConfig(middleware.RateLimiterConfig{
 		Store: middleware.NewRateLimiterMemoryStoreWithConfig(middleware.RateLimiterMemoryStoreConfig{
 			Rate:      1,
 			Burst:     1,
-			ExpiresIn: 10 * time.Second,
+			ExpiresIn: 30 * time.Second,
 		}),
-	}))
+	})
 
-	e.GET("/", func(c echo.Context) error { return c.File("statics/index.html") })
+	e.POST("/", h.HandleSubmit, ratelimit)
+
+	e.GET("/", func(c echo.Context) error { return c.File("statics/index.html") }, ratelimit)
 	e.File("/favicon.png", "statics/favicon.png")
 
 	if err := e.Start(":8080"); err != nil {
