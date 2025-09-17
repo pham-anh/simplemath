d5a3073ad222be936e329f157ed1bdc3971f2aad
Pham Quynh Anh <pqa.dev@gmail.com>
2025-09-17 21:13:07 +0900

    final touch

--- handler/formdata.go
+++ handler/formdata.go
@@ -9,46 +9,47 @@ import (
 )
 
 type FormData struct {
-    Operator     string `form:"operator"`
-    NumQuestions int    `form:"numQuestions"`
-    NumOperands  int    `form:"numOperands"`
-    Digits       []int  `form:"numDigits"`
+	Operator     string `form:"operator"`
+	NumQuestions int    `form:"numQuestions"`
+	NumOperands  int    `form:"numOperands"`
+	Digits       []int  `form:"numDigits"`
 }
 
 func (f *FormData) validate() error {
-    op := operator.Operator(f.Operator)
-    if f.Operator == "" || (op != operator.Addition && op != operator.Subtraction && op != operator.Multiplication && op != operator.Division) {
-        return fmt.Errorf("Invalid or missing operator")
-    }
-    if f.NumQuestions < 1 {
-        return fmt.Errorf("Number of Questions must be at least 1")
-    }
-    if f.NumOperands < 2 {
-        f.NumOperands = 2
-    }
-    if f.NumOperands > 3 {
-        f.NumOperands = 3
-    }
-    if len(f.Digits) != f.NumOperands {
-        return fmt.Errorf("Provide digits for each operand (%d)", f.NumOperands)
-    }
-    for _, d := range f.Digits {
-        if d < 1 {
-            return fmt.Errorf("Digits must be >= 1")
+	op := operator.Operator(f.Operator)
+	if f.Operator == "" || (op != operator.Addition && op != operator.Subtraction && op != operator.Multiplication && op != operator.Division) {
+		return fmt.Errorf("Phép toán không hợp lệ hoặc bị thiếu")
+	}
+	if f.NumQuestions < 1 {
+		return fmt.Errorf("Số lượng câu hỏi phải ít nhất là 1")
+	}
+	if f.NumOperands < 2 {
+		f.NumOperands = 2
+	}
+	if f.NumOperands > 3 {
+		f.NumOperands = 3
+	}
+	if len(f.Digits) != f.NumOperands {
+		return fmt.Errorf("Vui lòng cung cấp số chữ số cho mỗi số hạng (%d)", f.NumOperands)
+	}
+	for _, d := range f.Digits {
+		if d < 1 {
+			return fmt.Errorf("Số chữ số phải >= 1")
+		}
+        if d > 3 {
+            return fmt.Errorf("Số chữ số phải <= 3")
         }
-    }
-    return nil
+	}
+	return nil
 }
 
 func FormDataFromRequest(c echo.Context) (*FormData, error) {
-    var f FormData
-    if err := c.Bind(&f); err != nil {
-        return nil, err
-    }
-    if err := f.validate(); err != nil {
-        return nil, err
-    }
-    return &f, nil
+	var f FormData
+	if err := c.Bind(&f); err != nil {
+		return nil, err
+	}
+	if err := f.validate(); err != nil {
+		return nil, err
+	}
+	return &f, nil
 }
-
-
